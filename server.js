2e4f23815ec5907bfb588dd903b8006002810702
kpratik05 <kuradepratik@gmail.com>
2022-08-01 17:10:37 +0530

    New Files added with docker file

---
+++ server.js
@@ -0,0 +1,35 @@
+
+var express = require('express');
+var app = express();
+var path=require("path");
+var fs=require("fs");
+
+//server configuration
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(___dirname + '/index.html'));
+});
+ 
+app.get('/contact', function (req, res) {
+  res.sendFile(path.join(___dirname + '/contact.html'));
+});
+
+app.get('/aboutus', function (req, res) {
+  res.sendFile(path.join(___dirname + '/aboutus.html'));
+});
+
+app.get('/hello', function (req, res) {
+  console.log("CAlling rest api");
+  var person={firstName:'Ravi',lastName:'Tambade',age:47};
+  res.send(person);
+});
+
+ 
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at 8081");
+})
